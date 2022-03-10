5d6929e8c5dfc07c83233c0896092958875c8250
symphony <jciurc@gmail.com>
2022-03-10 05:00:12 +0000

    using while loop to create catchy tune

---
+++ chorus-while.js
@@ -0,0 +1,13 @@
+// tinker file for while loops
+
+// sing chorus x times
+const chorus = "Let's dance!";
+let repeat = 0;
+while (repeat <10) {
+    if (repeat === 5 ) { //
+        console.log("*key change*");
+    }
+    console.log(chorus);
+    repeat++
+}
+console.log("Until the Sun comes up!");
