4ddf922edb5850d5687ea15e64814c0bf336bcba
symphony <jciurc@gmail.com>
2022-03-15 02:08:21 +0000

    Completed all arrays assignments

---
+++ arrays-merge.js
@@ -0,0 +1,10 @@
+function merge(array1, array2) {
+  for (const element of array2) {
+    array1.push(element);
+  }
+  return array1.sort();
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
